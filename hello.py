0a337cf206077c6b72ac05fa849c3cc21928d871
Turbanov <tur.4000@gmail.com>
2018-08-12 22:02:54 +0300

    первая форма

--- hello.py
+++ hello.py
@@ -2,14 +2,23 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'e2e36e5a223f49459d96c67b7d1b9fe8'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html', 
+	name = None
+	form = NameForm()
+	if form.validate_on_submit():
+		name = form.name.data
+		form.name.data = ''
+	return render_template('index.html', form=form, name=name,
 							current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
@@ -23,3 +32,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
 	return render_template('500.html'), 500
+
+class NameForm(FlaskForm):
+	name = StringField('Как тебя зовут?', validators=[DataRequired()])
+	submit = SubmitField('Отправить')
