08a5e0cd5fef8b94ad987b2d3eb2ae3d91249f1b
Turbanov Valeriy <tur.4000@gmail.com>
2018-08-13 11:07:46 +0300

    tab to spaces

--- hello.py
+++ hello.py
@@ -13,32 +13,32 @@ moment = Moment(app)
 
 
 class NameForm(FlaskForm):
-	name = StringField('Как тебя зовут?', validators=[DataRequired()])
-	submit = SubmitField('Отправить')
+    name = StringField('Как тебя зовут?', validators=[DataRequired()])
+    submit = SubmitField('Отправить')
 
 
 @app.errorhandler(404)
 def page_not_found(e):
-	return render_template('404.html'), 404
+    return render_template('404.html'), 404
 
 @app.errorhandler(500)
 def internal_server_error(e):
-	return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
 @app.route('/user/<name>')
 def user(name):
-	return render_template('user.html', name=name)
+    return render_template('user.html', name=name)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = NameForm()
-	if form.validate_on_submit():
-		old_name = session.get('name')
-		if old_name is not None and old_name != form.name.data:
-			flash('Looks like you have changed your name!')
-		session['name'] = form.name.data
-		return redirect(url_for('index'))
-	return render_template('index.html', 
-							form=form, 
-							name=session.get('name'),
-							current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', 
+                            form=form, 
+                            name=session.get('name'),
+                            current_time=datetime.utcnow())
