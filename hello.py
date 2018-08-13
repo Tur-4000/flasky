9244b1a6f1533e0bc37c2c98781987ee61d1ace0
Turbanov Valeriy <tur.4000@gmail.com>
2018-08-13 15:47:05 +0300

    add Flask-Migration

--- hello.py
+++ hello.py
@@ -7,6 +7,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,6 +19,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.shell_context_processor
 def make_shell_context():
