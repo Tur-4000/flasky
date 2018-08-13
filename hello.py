24a2d9fadd20e3f9d0257a37def14a0ef845a724
Turbanov Valeriy <tur.4000@gmail.com>
2018-08-13 13:40:00 +0300

    small fix in Role model

--- hello.py
+++ hello.py
@@ -56,7 +56,7 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    users = db.relationship('Users', backref='role')
+    users = db.relationship('User', backref='role')
 
     def __repr__(self):
         return '<Role %r>' % self.name
