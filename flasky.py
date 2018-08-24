5f0080bf765c572a7528415d8d545a48389b7f84
Turbanov <tur.4000@gmail.com>
2018-08-25 00:44:50 +0300

    fin

--- flasky.py
+++ flasky.py
@@ -1,4 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
 
 COV = None
 if os.environ.get('FLASK_COVERAGE'):
@@ -13,7 +18,6 @@ from app.models import User, Role, Permission, Post, Follow, Comment
 from flask_migrate import Migrate, upgrade
 
 
-from config import Config
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 
